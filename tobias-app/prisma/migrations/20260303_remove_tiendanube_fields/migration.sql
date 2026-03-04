1c294eb629fccd9f4af54ce2dad3d0568cacaf02
Tobias Choclin <tchoclin@uade.edu.ar>
2026-03-04 16:35:47 -0300

    Remove duplicate directory

--- tobias-app/prisma/migrations/20260303_remove_tiendanube_fields/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "User" DROP COLUMN IF EXISTS "tiendanubeStoreId",
-DROP COLUMN IF EXISTS "tiendanubeUserId",
-DROP COLUMN IF EXISTS "tiendanubeAccessToken",
-DROP COLUMN IF EXISTS "tiendanubeTokenType",
-DROP COLUMN IF EXISTS "tiendanubeScope";
